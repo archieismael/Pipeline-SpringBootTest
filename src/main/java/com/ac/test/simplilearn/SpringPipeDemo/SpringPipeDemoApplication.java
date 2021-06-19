3692ca21a138d6c4620a93b3f0625fbfc3b59f0d
student <archie_ismael@yahoo.com>
2021-06-19 16:53:42 +0000

    SpringBoot Project Test

---
+++ src/main/java/com/ac/test/simplilearn/SpringPipeDemo/SpringPipeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ac.test.simplilearn.SpringPipeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipeDemoApplication.class, args);
+	}
+
+}
